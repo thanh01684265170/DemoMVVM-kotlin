b776651d2f7ca68d0d9dd8f42d706aeb0376c278
thanh <thanh01684265170@gmail.com>
2019-04-02 23:18:35 +0700

    databinding

--- app/src/main/java/com/example/h/screen/home/BookAdapter.kt
+++ app/src/main/java/com/example/h/screen/home/BookAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.h.screen.home
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.h.R
+import com.example.h.databinding.ItemBinding
 import com.example.h.model.Book
 import kotlinx.android.synthetic.main.item.view.*
-
 
 class BookAdapter(var c: Context, var lists: ArrayList<Book>) :
     ListAdapter<Book, BookAdapter.ViewHolder>(TaskDiffCallback()) {
@@ -19,13 +19,15 @@ class BookAdapter(var c: Context, var lists: ArrayList<Book>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = LayoutInflater.from(c).inflate(R.layout.item, parent, false)
-        return ViewHolder(view)
+//        val view: View = LayoutInflater.from(c).inflate(R.layout.item, parent, false)
+        val itemBinding: ItemBinding = DataBindingUtil
+            .inflate(LayoutInflater.from(c),R.layout.item, parent, false)
+        return ViewHolder(itemBinding)
     }
 
     override fun submitList(list: MutableList<Book>?) {
 
-       val listBook = ArrayList<Book>()
+        val listBook = ArrayList<Book>()
         if (listBook != null) {
             listBook.addAll(list ?: ArrayList<Book>())
         }
@@ -33,7 +35,7 @@ class BookAdapter(var c: Context, var lists: ArrayList<Book>) :
 
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemBinding: ItemBinding) : RecyclerView.ViewHolder(itemBinding) {
 
         fun bindData(book: Book) {
             itemView.text_name.text = book.ten
--- app/src/main/java/com/example/h/screen/home/MainActivity.kt
+++ app/src/main/java/com/example/h/screen/home/MainActivity.kt
@@ -1,37 +1,40 @@
 package com.example.h.screen.home
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
+import android.databinding.ObservableField
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.example.h.R
+import com.example.h.databinding.ActivityMainBinding
 import com.example.h.model.Book
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        var title: ObservableField<String> = ObservableField()
+        var bindding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        title.set("Phạm Quang thanh")
+        bindding.main
 
-        recyclerview.layoutManager = LinearLayoutManager(this)
-        recyclerview.hasFixedSize()
-        val adapter =  BookAdapter(this, getLists())
-        recyclerview.adapter = adapter
+        bindding.recyclerview.layoutManager = LinearLayoutManager(this)
+        bindding.recyclerview.hasFixedSize()
+        val adapter = BookAdapter(this, getLists())
+        bindding.recyclerview.adapter = adapter
 
         adapter.submitList(getLists())
 
     }
 
-
     fun getLists(): ArrayList<Book> {
         var lists = ArrayList<Book>()
-        lists.add(Book(1,"thanh",101))
-        lists.add(Book(2,"thanh",102))
-        lists.add(Book(3,"thanh",103))
-        lists.add(Book(4,"thanh",104))
-        lists.add(Book(5,"thanh",105))
-        lists.add(Book(6,"thanh",106))
+        lists.add(Book(1, "thanh", 101))
+        lists.add(Book(2, "thanh", 102))
+        lists.add(Book(3, "thanh", 103))
+        lists.add(Book(4, "thanh", 104))
+        lists.add(Book(5, "thanh", 105))
+        lists.add(Book(6, "thanh", 106))
         return lists;
     }
-
 }
